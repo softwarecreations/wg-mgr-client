1eca24a835562a7d07b4ce1d005a27e13fdc0813
Joshua Maserow <joshua@softwarecreations.co.za>
2024-08-21 14:35:04 +0200

    'Added colors'

--- wg-mgr-client.js
+++ wg-mgr-client.js
@@ -5,6 +5,8 @@ const https = require('https');
 const { exec, spawn } = require('child_process');
 const otherPossibleServicesA = [ 'ssh', 'nginx', 'mongod' ]; // they don't need to be installed on all systems, do not edit for specific systems.
 
+const colors = { red:s=>`\u001b[31m${s}\u001b[0m`, green:s=>`\u001b[32m${s}\u001b[0m`, yellow:s=>`\u001b[33m${s}\u001b[0m`, blue:s=>`\u001b[34m${s}\u001b[0m`, magenta:s=>`\u001b[35m${s}\u001b[0m`, cyan:s=>`\u001b[36m${s}\u001b[0m`, white:s=>`\u001b[37m${s}\u001b[0m`, black:s=>`\u001b[30m${s}\u001b[0m`, gray:s=>`\u001b[90m${s}\u001b[0m` };
+
 ( () => { // wrapped for uglify-js
   const urlFilePath = __dirname + '/wg-mgr-client.url';
   const clientHttpsUrl = fs.readFileSync(urlFilePath).toString().split('\n')[0].trim();
